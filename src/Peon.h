636e29db75518d9d5afc75a8d688b7561d5d97e1
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-05-31 18:19:11 +0200

    Update Peon.h

--- src/Peon.h
+++ src/Peon.h
@@ -3,12 +3,13 @@
 #include "Pieza.h"
 #include <vector>
 
+using namespace std;
 class Peon : public Pieza {
 public:
     Peon(int _x, int _y, Color _color, bool _esModoDemi);
     virtual bool esMovimientoValido(int nuevoX, int nuevoY) override;
-    virtual std::string obtenerNombre() const override;
-    bool esCapturaAlPaso(int nuevoX, int nuevoY, const std::vector<std::vector<Pieza*>>& casillas) const;
+    virtual string obtenerNombre() const override;
+    bool esCapturaAlPaso(int nuevoX, int nuevoY, const vector<vector<Pieza*>>& casillas) const;
     bool esPrimerMovimiento() const;
 
 private:
