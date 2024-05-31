3d8bda9471c7f4b950e049c24a522c3ccde590ca
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-05-31 18:21:23 +0200

    Update Caballo.cpp

--- src/Caballo.cpp
+++ src/Caballo.cpp
@@ -5,8 +5,8 @@ Caballo::Caballo(int _x, int _y, Color _color) : Pieza(_x, _y, _color) {}
 
 bool Caballo::esMovimientoValido(int nuevoX, int nuevoY) {
     // El caballo se mueve en forma de L: dos casillas en una dirección y una casilla en la otra
-    int dx = std::abs(nuevoX - x);
-    int dy = std::abs(nuevoY - y);
+    int dx = abs(nuevoX - x);
+    int dy = abs(nuevoY - y);
     return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
 }
 
