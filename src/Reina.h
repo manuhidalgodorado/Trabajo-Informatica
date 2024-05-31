339943f30b33a193e42f94020bbe9748342edd0e
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-05-31 18:15:21 +0200

    Update Reina.h

--- src/Reina.h
+++ src/Reina.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "Pieza.h"
-
+using namespace std;
 class Reina : public Pieza {
 public:
     Reina(int _x, int _y, Color _color);
     virtual bool esMovimientoValido(int nuevoX, int nuevoY) override;
-    virtual std::string obtenerNombre() const override;
+    virtual string obtenerNombre() const override;
 };
 
