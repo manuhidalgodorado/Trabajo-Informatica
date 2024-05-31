8b462cf10cc6be9761e6f6a1f60c8c76d11bb3ae
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-05-31 18:15:50 +0200

    Update Reina.cpp

--- src/Reina.cpp
+++ src/Reina.cpp
@@ -1,13 +1,14 @@
 #include "Reina.h"
 #include <cmath>
 
+using namespace std;
 Reina::Reina(int _x, int _y, Color _color) : Pieza(_x, _y, _color) {}
 
 bool Reina::esMovimientoValido(int nuevoX, int nuevoY) {
     return (x == nuevoX || y == nuevoY || std::abs(nuevoX - x) == std::abs(nuevoY - y));
 }
 
-std::string Reina::obtenerNombre() const {
+string Reina::obtenerNombre() const {
     return "Reina";
 }
 
