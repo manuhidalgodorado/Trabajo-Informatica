6314e95829aa7a1ffb681ad4130a0ff67945609b
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-05-31 18:14:13 +0200

    Update Caballo.h

--- src/Caballo.h
+++ src/Caballo.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "Pieza.h"
+using namespace std;
 
 class Caballo : public Pieza {
 public:
     Caballo(int _x, int _y, Color _color);
     virtual bool esMovimientoValido(int nuevoX, int nuevoY) override;
-    virtual std::string obtenerNombre() const override;
+    virtual string obtenerNombre() const override;
 };
 
 
