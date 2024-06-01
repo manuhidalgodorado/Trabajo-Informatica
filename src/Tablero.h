054dfb5b067e062de1d0065238d482c73140d748
manuhidalgodorado <163344671+manuhidalgodorado@users.noreply.github.com>
2024-06-01 18:54:01 +0200

    Update Tablero.h

--- src/Tablero.h
+++ src/Tablero.h
@@ -1,2 +1,21 @@
+#pragma once
 
+#include <vector>
+#include "Pieza.h"
+
+class Tablero {
+protected:
+    std::vector<std::vector<Pieza*>> casillas;
+    Color turno;
+    int seleccionadoX, seleccionadoY;
+
+public:
+    Tablero();
+    virtual ~Tablero();
+
+    int getSeleccionadoX() const;
+    int getSeleccionadoY() const;
+    void setSeleccionadoX(int x);
+    void setSeleccionadoY(int y);
+};
 
