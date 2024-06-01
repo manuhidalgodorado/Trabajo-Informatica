c65d897b04a572ae2edc50fabd136dcdaead7588
manuhidalgodorado <163344671+manuhidalgodorado@users.noreply.github.com>
2024-06-01 18:57:02 +0200

    Update Tablero.cpp

---
+++ src/Tablero.cpp
@@ -0,0 +1,36 @@
+#include "Tablero.h"
+#include "Rey.h"
+#include "Reina.h"
+#include "Torre.h"
+#include "Peon.h"
+#include "Caballo.h"
+#include "Alfil.h"
+#include <iostream>
+#include <freeglut.h>
+#include <string>
+#include <vector>
+
+Tablero::Tablero() : turno(BLANCO), seleccionadoX(-1), seleccionadoY(-1) {}
+Tablero::~Tablero() {
+    for (auto& fila : casillas) {
+        for (auto& pieza : fila) {
+            delete pieza;
+        }
+    }
+}
+
+int Tablero::getSeleccionadoX() const {
+    return seleccionadoX;
+}
+
+int Tablero::getSeleccionadoY() const {
+    return seleccionadoY;
+}
+
+void Tablero::setSeleccionadoX(int x) {
+    seleccionadoX = x;
+}
+
+void Tablero::setSeleccionadoY(int y) {
+    seleccionadoY = y;
+}
