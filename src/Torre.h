26309cbc4eee64d927924364b4fb2de39f9a17d9
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-06-01 18:25:15 +0200

    Update Torre.h

--- src/Torre.h
+++ src/Torre.h
@@ -5,7 +5,7 @@ class Torre : public Pieza {
 public:
     Torre(int _x, int _y, Color _color);
     virtual bool esMovimientoValido(int nuevoX, int nuevoY) override;
-    virtual std::string obtenerNombre() const override;
+    virtual string obtenerNombre() const override;
 };
 
 
