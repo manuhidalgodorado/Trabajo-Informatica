68be9fd96661cd77e53461481832d0fbaee584a6
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-06-01 18:26:15 +0200

    Update Torre.cpp

--- src/Torre.cpp
+++ src/Torre.cpp
@@ -1,12 +1,13 @@
 #include "Torre.h"
 
+using namespace std;
 Torre::Torre(int _x, int _y, Color _color) : Pieza(_x, _y, _color) {}
 
 bool Torre::esMovimientoValido(int nuevoX, int nuevoY) {
     return (x == nuevoX || y == nuevoY);
 }
 
-std::string Torre::obtenerNombre() const {
+string Torre::obtenerNombre() const {
     return "Torre";
 }
 
