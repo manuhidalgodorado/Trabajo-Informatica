ef45b78a24339002ce65ac020ed63c59303da797
manueldelgado <163348340+xmanu14hd@users.noreply.github.com>
2024-06-02 11:56:35 +0200

    Update Tablero.cpp

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-Tablero::Tablero(bool Demi) : turno(BLANCO), seleccionadoX(-1), seleccionadoY(-1) Demi(es_Demi) {}
+Tablero::Tablero(bool esModoDemi) : turno(BLANCO), seleccionadoX(-1), seleccionadoY(-1) esModoDemi(_esModoDemi) {}
 Tablero::~Tablero() {
     for (auto& fila : casillas) {
         for (auto& pieza : fila) {
