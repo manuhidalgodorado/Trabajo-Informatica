1c77a54bfdb3f57382431739d6edec8f5cfd5dca
EdeAntonio <enriquedeanoniosanchez@gmail.com>
2024-06-06 00:21:15 +0200

    Metodo para hacer y seleccionar la coronacion

--- src/Tablero.h
+++ src/Tablero.h
@@ -26,7 +26,7 @@ public:
     bool moverPieza(int xInicial, int yInicial, int xFinal, int yFinal);
     bool esCapturaAlPaso(int xInicial, int yInicial, int xFinal, int yFinal) const;
     vector<pair<int, int>> obtenerMovimientosPosibles(int x, int y);
-    pair<int, int> peonCorona();
+    pair<int, int> peonCorona(bool& coronacion);
     bool hayJaque();
     bool hayMate_Ahogado();
     void resetearPassant(Color turno);
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ bool menuActivo = true;
 Tablero* tablero = nullptr;
 int mainWindow;
 std::pair<int, int> peonCorona;
+char a;
+bool coronacion;
 
 void displayText(float x, float y, const char* text) {
     glRasterPos2f(x, y);
@@ -121,10 +123,47 @@ void onMouseClick(int button, int state, int x, int y) {
                 int seleccionadoX = tablero->getSeleccionadoX();
                 int seleccionadoY = tablero->getSeleccionadoY();
                 if (tablero->moverPieza(seleccionadoX, seleccionadoY, tableroY, tableroX)) {
-                    peonCorona=tablero->peonCorona();
-                    tablero->cambiarTurno();
-                    tablero->setSeleccionadoX(-1);
-                    tablero->setSeleccionadoY(-1);
+                    peonCorona = tablero->peonCorona(coronacion);
+                    if (coronacion)
+                    {
+                        if (tipoJuego == 2)
+                        {
+                            while (a != 'c' && a != 'a' && a != 't')
+                            {
+                                std::cout << (char)168 << "A que quieres promover?: ";
+                                std::cin >> a;
+                                if (a != 'c' && a != 'a' && a != 't')
+                                {
+                                    std::cout << std::endl << "Seleccione entre Torre: t, Alfil: a, Caballo: c" << std::endl;
+                                }
+                                else
+                                {
+                                    std::cout << std::endl << "Coronacion correcta, vuelva al juego" << std::endl;
+                                }
+                            }
+                        }
+                        if (tipoJuego == 1)
+                        {
+                            while (a != 't' && a != 'r')
+                            {
+                                std::cout << (char)168 << "A que quieres promover?: ";
+                                std::cin >> a;
+                                if (a != 't' && a != 'r')
+                                {
+                                    std::cout << std::endl << "Seleccione entre Torre: t, Reina: r" << std::endl;
+                                }
+                                else
+                                {
+                                    std::cout << std::endl << "Coronacion correcta, vuelva al juego" << std::endl;
+                                }
+                            }
+                        }
+                    }
+                    if (!coronacion) {
+                        tablero->cambiarTurno();
+                        tablero->setSeleccionadoX(-1);
+                        tablero->setSeleccionadoY(-1);
+                    }
                 }
                 else {
                     tablero->setSeleccionadoX(-1);
