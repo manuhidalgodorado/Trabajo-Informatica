bd2a460b932c8810987bb78434ac88f9e0633f35
EdeAntonio <enriquedeanoniosanchez@gmail.com>
2024-06-05 13:53:53 +0200

    Actualizacion clase peon para crear funcion de captura en paso

--- src/Peon.cpp
+++ src/Peon.cpp
@@ -3,26 +3,25 @@
 #include <vector>
 
 using namespace std;
-Peon::Peon(int _x, int _y, Color _color, bool _esModoDemi) : Pieza(_x, _y, _color), esModoDemi(_esModoDemi) {}
+
+Peon::Peon(int _x, int _y, Color _color, bool _esModoDemi) : Pieza(_x, _y, _color), esModoDemi(_esModoDemi) {
+    enPassant = false;
+}
 
 bool Peon::esMovimientoValido(int nuevoX, int nuevoY) {
     int dir = (color == BLANCO) ? 1 : -1;
-
     // Movimiento hacia adelante
     if (nuevoX == x + dir && nuevoY == y) {
         return true;
     }
-
     // Movimiento inicial de dos casillas solo en modo Demi
     if (esModoDemi && esPrimerMovimiento() && nuevoX == x + 2 * dir && nuevoY == y) {
         return true;
     }
-
     // Captura en diagonal
-    if (nuevoX == x + dir && std::abs(nuevoY - y) == 1) {
+    if (nuevoX == x + dir && abs(nuevoY - y) == 1) {
         return true;
     }
-
     return false;
 }
 
@@ -30,16 +29,18 @@ string Peon::obtenerNombre() const {
     return "Peon";
 }
 
-bool Peon::esCapturaAlPaso(int nuevoX, int nuevoY, const vector<vector<Pieza*>>& casillas) const {
+bool Peon::esCapturaAlPaso(int nuevoX, int nuevoY, const std::vector<std::vector<Pieza*>>& casillas) const {
     if (!esModoDemi) return false;  // Captura al paso solo en modo Demi
     int dir = (color == BLANCO) ? 1 : -1;
-    if (nuevoX == x + dir && std::abs(nuevoY - y) == 1) {
+    if (nuevoX == x + dir && abs(nuevoY - y) == 1) {
         // Verificar si la captura al paso es posible
         int peonX = x;
         int peonY = nuevoY;
         Pieza* posiblePeon = casillas[peonX][peonY];
+        Peon* PeonConfirmado;
         if (posiblePeon != nullptr && dynamic_cast<Peon*>(posiblePeon) && posiblePeon->getColor() != color) {
-            return true;
+            PeonConfirmado = dynamic_cast<Peon*>(posiblePeon);
+            if (PeonConfirmado->getPassant()) return true;
         }
     }
     return false;
@@ -47,6 +48,14 @@ bool Peon::esCapturaAlPaso(int nuevoX, int nuevoY, const vector<vector<Pieza*>>&
 
 bool Peon::esPrimerMovimiento() const {
     return (color == BLANCO && x == 1) || (color == NEGRO && x == 6);
+}
+
+void Peon::setEnPassant(bool Passant) {
+    enPassant = Passant;
+}
+bool Peon::getPassant()
+{
+    return enPassant;
 }
 
 
--- src/Peon.h
+++ src/Peon.h
@@ -4,18 +4,22 @@
 #include <vector>
 
 using namespace std;
+
 class Peon : public Pieza {
 public:
     Peon(int _x, int _y, Color _color, bool _esModoDemi);
     virtual bool esMovimientoValido(int nuevoX, int nuevoY) override;
     virtual string obtenerNombre() const override;
-    bool esCapturaAlPaso(int nuevoX, int nuevoY, const vector<vector<Pieza*>>& casillas) const;
+    bool esCapturaAlPaso(int nuevoX, int nuevoY, const std::vector<std::vector<Pieza*>>& casillas) const;
     bool esPrimerMovimiento() const;
+
+    void setEnPassant(bool PosPassant);
+    bool getPassant();
 
 private:
     bool esModoDemi;
+    bool enPassant;
 };
-
 
 
 
